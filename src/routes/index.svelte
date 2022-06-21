4890a6cf03b320e737ddfa85bc0339fef2051714
Joseph Bravo <josephangelobravo@gmail.com>
2022-06-21 06:55:57 -0700

    post method finished

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  let todos = [];
+  let value = '';
+
+  function handleSubmit(event) {
+    console.log(value);
+    fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        entry: value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        todos = [...todos, res];
+      })
+      .catch(console.error);
+    event.target.reset();
+  }
+</script>
+
+<h1>Todo-List</h1>
+<ul>
+  {#each todos as todo}
+    <li>{todo.entry}</li>
+  {/each}
+</ul>
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" bind:value required />
+  <button type="submit">Add</button>
+</form>
